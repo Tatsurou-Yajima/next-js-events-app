d8a76d99113b1c5a12d25ebcf186a63a2e554911
T.Yajima <tatsurou.yajima@gmail.com>
2023-07-09 10:37:18 +0900

    💫 イベントページを実装

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,24 +1,26 @@
-export default function EventsPage() {
+import { events_categories } from '/data/data.json'
+import Image from 'next/image'
+
+export default function EventsPage({ data }) {
   return (
     <div>
       <h1>Event page</h1>
       <div>
-        <a href=''>
-          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-            <h2>Event In Tokyo</h2>
-          </div>
-        </a>
-        <a href=''>
-          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-            <h2>Event In Kyoto</h2>
-          </div>
-        </a>
-        <a href=''>
-          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-            <h2>Event In Kanagawa</h2>
-          </div>
-        </a>
+        {data.map(ev => (
+          <a key={ev.id} href={`/events/${ev.id}`}>
+            <Image src={ev.image} width={300} height={200} alt={ev.title} />
+            <h2>{ev.title}</h2>
+          </a>
+        ))}
       </div>
     </div>
   )
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      data: events_categories
+    }
+  }
 }
