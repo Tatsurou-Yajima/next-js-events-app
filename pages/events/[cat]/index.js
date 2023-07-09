e7ca6b484b681858c216f4977091e2612bee75a4
T.Yajima <tatsurou.yajima@gmail.com>
2023-07-09 11:16:50 +0900

    💫 都市ごとのイベントページを実装

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -1,10 +1,42 @@
-const EventsCatPage = () => {
+import { allEvents, events_categories } from '/data/data.json'
+import Image from 'next/image'
+
+const EventsCatPage = ({ data }) => {
     return <div>
         <h1>Events</h1>
-        <a href="/event/event1">Event 1</a>
-        <a href="/event/event2">Event 2</a>
-        <a href="/event/event3">Event 3</a>
+        <div>
+            {data.map(ev => (
+                <a key={ev.id} href={`/event/${ev.id}`}>
+                    <Image src={ev.image} width={300} height={200} alt={ev.title} />
+                    <h2>{ev.title}</h2>
+                    <p>{ev.description}</p>
+                </a>
+            ))}
+        </div>
     </div>
 }
 
 export default EventsCatPage;
+
+export async function getStaticPaths() {
+    const allPaths = events_categories.map(ev => {
+        return {
+            params: {
+                cat: ev.id.toString(),
+            }
+        }
+    })
+
+    return {
+        paths: allPaths,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps(context) {
+    const id = context?.params.cat;
+    const data = allEvents.filter(ev => ev.city === id);
+    return {
+        props: { data }
+    }
+}
