317d88d81d770eece870e66bff283472b87cd0f8
T.Yajima <tatsurou.yajima@gmail.com>
2023-07-22 09:50:10 +0900

    ✨ ヘッダーのスタイルを調整

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,30 @@
 import Link from "next/link"
+import Image from "next/image"
+
 export const Header = () => {
     return (
         <header>
-            <nav>
-                <Link href="/">Home</Link>
-                <Link href="/events">Events</Link>
-                <Link href="/about-us">About Us</Link>
-            </nav>
+            <div className="topNav">
+                <Image src={'/images/logo_black.png'} width={50} height={50} />
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <Link href="/events">Events</Link>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <Link href="/about-us">About Us</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <h1>Ut enim ad minima veniam, quis nostrum exercitationem</h1>
         </header>
     )
 }
