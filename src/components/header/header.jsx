e422daffc179375966829a34d445f7f0c04b9c23
T.Yajima <tatsurou.yajima@gmail.com>
2023-07-23 10:29:25 +0900

    💫 About usページのCSS調整

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,27 +4,29 @@ import Image from "next/image"
 export const Header = () => {
     return (
         <header>
-            <div className="topNav">
-                <Image src={'/images/logo_black.png'} width={50} height={50} />
-                <nav>
-                    <ul>
-                        <li>
-                            <Link href="/">Home</Link>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <Link href="/events">Events</Link>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <Link href="/about-us">About Us</Link>
-                        </li>
-                    </ul>
-                </nav>
+            <div>
+                <div className="topNav">
+                    <Image src={'/images/logo_black.png'} alt="logo" width={50} height={50} />
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link href="/">Home</Link>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <Link href="/events">Events</Link>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <Link href="/about-us">About Us</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <h1>Ut enim ad minima veniam</h1>
             </div>
-            <h1>Ut enim ad minima veniam</h1>
         </header>
     )
 }
