08beefd27f485c8cdb9148a458edcb4d9af7721b
T.Yajima <tatsurou.yajima@gmail.com>
2023-07-29 09:41:00 +0900

    🔧イベントページ微修正

--- src/components/events/events-page.jsx
+++ src/components/events/events-page.jsx
@@ -4,11 +4,10 @@ import Image from 'next/image'
 
 const AllEvents = ({ data }) => {
     return (
-        <div className='events_page'>
-            <h1>Event page</h1>
-            {data.map((ev) => (
-                <Link key={ev.id} href={`/events/${ev.id}`} passHref className='card'>
-                    <Image src={ev.image} width={500} height={500} alt={ev.title} />
+        <div className="events_page">
+            {data?.map((ev) => (
+                <Link key={ev.id} href={`/events/${ev.id}`} passHref className="card">
+                    <Image src={ev.image} width={300} height={300} alt={ev.title} />
                     <h2>{ev.title}</h2>
                 </Link>
             ))}
