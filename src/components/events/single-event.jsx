eb4926e4eee182be527c6b4dd9d5b367db611aac
T.Yajima <tatsurou.yajima@gmail.com>
2023-07-29 10:21:09 +0900

    💫メール送信のためのフォームのスタイルを調整

--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import Image from 'next/image'
 
 const SingleEvent = ({ data }) => {
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        const email = formData.get('email')
+    }
+
     return (
-        <div>
-            <Image src={data.image} width={600} height={500} alt={data.title} />
+        <div className='event_single_page'>
             <h1>{data.title}</h1>
+            <Image src={data.image} width={600} height={500} alt={data.title} />
             <p>{data.description}</p>
-            <input type='email' placeholder='Email Address' />
+            <form onSubmit={onSubmit} className='email_registration'>
+                <label>メーリングリストに登録する</label>
+                <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    placeholder='メールアドレスを入力してください'
+                />
+                <button type='button'>Submit</button>
+            </form>
         </div>
     )
 }
